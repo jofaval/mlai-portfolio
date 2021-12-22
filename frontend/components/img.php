4ebee9435e4ad5fdbd3de6de96625a257ffede8c
pepe_fabra_valverde <jofaval@outlook.com>
2021-12-22 23:31:12 +0100

    Add id to img component

--- frontend/components/img.php
+++ frontend/components/img.php
@@ -2,6 +2,7 @@
 
 if (!isset($src)) $src = '';
 if (!isset($alt)) $alt = $src;
+if (!isset($id)) $id = $alt;
 if (!isset($title)) $title = $alt;
 if (!isset($class)) $class = '';
 if (!isset($label)) $label = "An image of $title";
@@ -11,9 +12,9 @@ if (!isset($figure)) $figure = false;
 
 <?php if ($figure): ?>
     <figure>
-        <img src="<?= $src ?>" alt="<?= $alt ?>" class="img <?= $class ?>" aria-label="<?= $label ?>" title="<?= $title ?>">
+        <img src="<?= $src ?>" alt="<?= $alt ?>" class="img <?= $class ?>" aria-label="<?= $label ?>" title="<?= $title ?>" id="<?= $id ?>">
         <figcaption><?= $label ?></figcaption>
     </figure>
 <?php else: ?>
-    <img src="<?= $src ?>" alt="<?= $alt ?>" class="img <?= $class ?>" aria-label="<?= $label ?>" title="<?= $title ?>">
+    <img src="<?= $src ?>" alt="<?= $alt ?>" class="img <?= $class ?>" aria-label="<?= $label ?>" title="<?= $title ?>" id="<?= $id ?>">
 <?php endif; ?>
