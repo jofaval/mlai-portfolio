ccc62b062c0ac6c3dad5ff94746a918cc6b19506
pepe_fabra_valverde <jofaval@outlook.com>
2021-12-23 14:28:27 +0100

    Add img dir to public url generation

--- frontend/library.php
+++ frontend/library.php
@@ -49,7 +49,8 @@ function get_public_url(string $path): string
 {
     // Replace path with the public domain
     if (str_starts_with($path, PUBLIC_DIR)) $path = str_replace(PUBLIC_DIR, APP_URL, $path);
-    if (str_starts_with($path, BUILD_DIR)) $path = str_replace(BUILD_DIR, APP_URL, $path);
+    else if (str_starts_with($path, BUILD_DIR)) $path = str_replace(BUILD_DIR, APP_URL, $path);
+    else if (str_starts_with($path, IMG_DIR)) $path = str_replace(IMG_DIR, APP_URL, $path);
     else $path = path_join(APP_URL, $path);
     // Replace the path separator
     $path = str_replace(DIRECTORY_SEPARATOR, '/', $path);
